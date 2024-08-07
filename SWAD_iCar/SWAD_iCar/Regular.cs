55e8e4690f9554c91dc7ebc8cef82399d6843886
Vincent <s10257433@connect.np.edu.sg>
2024-08-07 22:18:58 +0800

    Updated prime and regular classes

--- SWAD_iCar/SWAD_iCar/Regular.cs
+++ SWAD_iCar/SWAD_iCar/Regular.cs
@@ -2,8 +2,8 @@ using System;
 
 public class Regular : Renter
 {
-    public Regular(int id, string name, string username, Card card, DateTime dateOfBirth, int contact, string bookingHistory, bool isVerified, string password, string email)
-        : base(id, name, username, card, dateOfBirth, contact, bookingHistory, isVerified, password, email)
+    public Regular(int id, DateTime dateOfBirth, int contact, string bookingHistory, bool isVerified, string password, string email, Booking[] makes, Admin isVerifiedBy, DigitalWallet has)
+        : base(id, dateOfBirth, contact, bookingHistory, isVerified, password, email, makes, isVerifiedBy, has)
     {
         // Additional initialization for Regular renters can be added here.
     }
