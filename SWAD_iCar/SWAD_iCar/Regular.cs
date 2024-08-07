1d5c75e4b4d5b8f1383e48d4c9fc621ddeb6cbc9
Vincent <s10257433@connect.np.edu.sg>
2024-08-07 21:53:18 +0800

    Updated, Admin, Prime, Regular, Report, and User

--- SWAD_iCar/SWAD_iCar/Regular.cs
+++ SWAD_iCar/SWAD_iCar/Regular.cs
@@ -1,4 +1,11 @@
 using System;
 
-public class Regular : Renter  {
+public class Regular : Renter
+{
+    public Regular(int id, string name, string username, Card card, DateTime dateOfBirth, int contact, string bookingHistory, bool isVerified, string password, string email)
+        : base(id, name, username, card, dateOfBirth, contact, bookingHistory, isVerified, password, email)
+    {
+        // Additional initialization for Regular renters can be added here.
+    }
+
 }
