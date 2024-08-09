d921bf86282907853d0092c4436f9d7c89450ee4
Hendrik Yong <hendrikyong1205@gmail.com>
2024-08-09 22:57:23 +0800

    UI functions

---
+++ SWAD_iCar/SWAD_iCar/UI-RegisterCar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD_iCar
+{
+    internal class UI_RegisterCar
+    {
+        private CTL_RegisterCar ctlRegisterCar;
+
+        public UI_RegisterCar()
+        {
+            ctlRegisterCar = new CTL_RegisterCar();
+        }
+
+        public void addNewCar()
+        {
+            displayRegisterCarForm();
+        }
+
+        public void displayRegisterCarForm()
+        {
+
+        }
+
+        public void addNewCar(string make, string model, int year, float mileage, string licensePlate, List<string> photos, Insurance insuranceDetails)
+        {
+
+        }
+
+        public bool promptConfirmation()
+        {
+            return true;
+        }
+
+        public void submitConfirmation()
+        {
+
+        }
+    }
+}
