925d08c0f0f1b2f1fca0a7b7f64aca467153df15
Shskjssk10 <cadentohjunyi@gmail.com>
2024-08-10 23:16:35 +0800

    Successfully Implemented Sequence Diagram

--- SWAD_iCar/SWAD_iCar/CTL-ManageCar.cs
+++ SWAD_iCar/SWAD_iCar/CTL-ManageCar.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SWAD_iCar
 {
@@ -12,7 +14,10 @@ namespace SWAD_iCar
         private CarOwner carOwner;
         private List<Car> carOwnerCars;
         private Car selectedCar;
-        
+        private List<Timeslot> registeredTimeslots;
+        private List<Timeslot> selectedTimeslots;
+
+
         public int validateOption(int option, int max)
         {
             if (option > max || option < 0)
@@ -50,45 +55,65 @@ namespace SWAD_iCar
         {
             return validateOption(option, 3);
         }
-        public void getTimeSlots()
+        public List<Timeslot> getTimeSlots()
         {
-
+            registeredTimeslots = selectedCar.GetTimeSlots();
+            return registeredTimeslots;
         }
-        public void selectTimeslots(int startID, int endID)
+        public List<Timeslot> selectTimeslots(int startID, int endID)
         {
-
+            selectedTimeslots = registeredTimeslots.Where(timeslot => timeslot.Id >= startID && timeslot.Id <= endID).ToList();
+            return selectedTimeslots;
         }
-        public void availabilityOrRateChange(int option)
+
+        public bool removeTimeslots(int startID, int endID)
         {
-
+            bool flag = false;
+            foreach (Timeslot timeslot in selectedTimeslots)
+            {
+                flag = selectedCar.RemoveTimeSlots(startID, endID);
+            }
+            return flag;
         }
-        public void inputNewAvailability(bool newStatus)
+        public int availabilityOrRateChange(int option)
         {
-
+            return validateOption(option, 2);
         }
-        public void displayTimeslots(List<Timeslot> selectedTimeslots)
+        public bool inputNewAvailability(int newStatus)
         {
-
+            newStatus = validateOption(newStatus, 2);
+            if (newStatus == 0)
+            {
+                return false;
+            }
+            selectedCar.UpdateTimeSlotsAvailability(selectedTimeslots, newStatus);
+            return true;
         }
-        public void inputNewRentalRate(float newRate)
+        public float inputNewRentalRate(float newRate)
         {
-
+            if (newRate <= 0)
+            {
+                return 0;
+            }
+            selectedCar.UpdateRentalRate(newRate);
+            return newRate; 
         }
+
+        // Still need fixing
         public DateTime? inputNewTimeslotsDateTill(DateTime inputDate)
         {
             DateTime currentDate = DateTime.Now.Date;
             if (inputDate <= currentDate) // Only show error message if there was previous input
             {
-                Console.WriteLine("Error1");
                 return null;
             }
             DateTime processedDate = inputDate.Date;
             processedDate = processedDate.AddHours(23).AddMinutes(59);
             currentDate = currentDate.AddHours(24);
-            while (currentDate <= processedDate)
+            while (currentDate < processedDate)
             {
-                currentDate = currentDate.AddHours(1);
                 selectedCar.AddNewTimeSlot(new Timeslot(currentDate, true, selectedCar));
+                currentDate = currentDate.AddHours(1);
             }
             return processedDate;
         }
